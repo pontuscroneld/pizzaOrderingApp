870690bf71e5a18c66778b10c9da3693d4583e7c
pontuscroneld <pontuscroneld@gmail.com>
2021-08-03 18:32:24 +0200

    Pressing a pizza place prints the name.

--- PizzaOrderingApp/Models/ApiHandler.swift
+++ PizzaOrderingApp/Models/ApiHandler.swift
@@ -7,7 +7,33 @@
 
 import Foundation
 
-public class ApiHandler {
+class ApiHandler {
 
-  
+  typealias CompletionHandler = (Result<[Restaurant], ApiError>) -> Void
+  var downloadedRestaurants: [Restaurant] = []
+
+  func loadRestaurants(completionHandler: @escaping CompletionHandler) {
+    let url = URL(string: "https://private-anon-94d7d533ab-pizzaapp.apiary-mock.com/restaurants/")!
+    var request = URLRequest(url: url)
+    let task = URLSession.shared.dataTask(with: request) { [self] data, response, error in
+      if let response = response {
+        if let data = data, let body = String(data: data, encoding: .utf8) {
+        }
+      } else {
+        print(error ?? "Unknown error")
+        completionHandler(.failure(ApiError.downloadFailed))
+      }
+      let response = try! JSONDecoder().decode([Restaurant].self, from: data!)
+      for pizzaPlace in response {
+        print(pizzaPlace.name)
+        self.downloadedRestaurants.append(pizzaPlace)
+      }
+      completionHandler(.success(self.downloadedRestaurants))
+    }
+    task.resume()
+  }
+}
+
+enum ApiError: Error {
+  case downloadFailed
 }
--- PizzaOrderingApp/Views/ListView.swift
+++ PizzaOrderingApp/Views/ListView.swift
@@ -9,15 +9,105 @@ import UIKit
 
 class ListView: UIViewController {
 
+  typealias DataSource = UICollectionViewDiffableDataSource<Section, Restaurant>
+  typealias DataSourceSnapShot = NSDiffableDataSourceSnapshot<Section, Restaurant>
+
+  var dataSource: DataSource!
+  var snapshot = DataSourceSnapShot()
+
   var apiHandler: ApiHandler?
   var collectionView: UICollectionView! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+      navigationItem.title = "Pizza places"
+
       apiHandler = ApiHandler()
-      view.backgroundColor = .systemRed
-        // Do any additional setup after loading the view.
+      configureCollectionViewLayout()
+      configureCollectionViewDataSource()
+      downloadRestaurants()
     }
+
+  func downloadRestaurants(){
+    self.apiHandler?.loadRestaurants() { [weak self] result in
+      DispatchQueue.main.async {
+          switch result {
+          case let .success(downloaded):
+            self?.applySnapshot(restaurants: downloaded)
+          case .failure(_):
+            self?.applySnapshot(restaurants: (self?.createDummyData())!)
+          }
+      }
+    }
+  }
 }
 
+// MARK: - Collection View Delegate
+extension ListView: UICollectionViewDelegate  {
+
+  enum Section {
+      case main
+  }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+      guard let chosenRestaurant = dataSource.itemIdentifier(for: indexPath) else { return }
+      print(chosenRestaurant.name)
+    }
+
+  func createLayout() -> UICollectionViewLayout {
+
+    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+    item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+    let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(76))
+    let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+    let section = NSCollectionLayoutSection(group: group)
+    let layout = UICollectionViewCompositionalLayout(section: section)
+
+    return layout
+  }
+
+  func configureCollectionViewLayout() {
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+    collectionView.delegate = self
+    collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    collectionView.backgroundColor = .systemBackground
+    collectionView.register(RestaurantListCell.self, forCellWithReuseIdentifier: RestaurantListCell.reuseIdentifier)
+    view.addSubview(collectionView)
+  }
+
+  func configureCollectionViewDataSource() {
+    dataSource = DataSource(
+      collectionView: collectionView,
+      cellProvider: { (collectionView, indexPath, restaurant) -> RestaurantListCell? in
+        let cell = collectionView.dequeueReusableCell(
+          withReuseIdentifier: RestaurantListCell.reuseIdentifier, for: indexPath) as! RestaurantListCell
+        cell.configure(with: restaurant)
+        return cell
+      })
+  }
+
+  func createDummyData() -> [Restaurant] {
+      var dummyData: [Restaurant] = []
+      for i in 0..<3 {
+          dummyData.append(
+            Restaurant(
+              id: i,
+              name: "Unknown Restaurant",
+              address1: "",
+              address2: "",
+              latitude: 0.0,
+              longitude: 0.0)
+          )
+      }
+    return dummyData
+  }
+
+  func applySnapshot(restaurants: [Restaurant]) {
+    snapshot = DataSourceSnapShot()
+    snapshot.appendSections([Section.main])
+    snapshot.appendItems(restaurants)
+    dataSource.apply(snapshot)
+  }
+}
